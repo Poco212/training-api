af9e0e9689c682ac3969b2e52ec254ecda19c4ec
poco212 <baihaqia442@gmail.com>
2026-04-04 06:46:55 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const api = 'https://a.4cdn.org/po/threads.json'
+const kali = document.getElementById("data")
 fetch(api)
 .then(response => response.json())
 .then(data => {
     data.forEach(data => {
         data.threads.forEach(data => {
-            console.log(data.last_modified)
+            kali.innerHTML += data.last_modified + "</br>"
         })
     })    
     });
-
-document.getElementById("data").innerHTML = "assoy"
